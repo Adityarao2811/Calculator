2aaed3a42304bbe9c8206e9d8d606d87a46ae377
Varsha Vinod <vvinod@affinionds.com>
2021-11-15 16:07:24 +0530

    Test method

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,30 +6,44 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            string equation = "_51*10=4500";
+            Test("42*47=1_74", 9);
+            Test("4_*47=1974", 2);
+            Test("42*_7=1974", 4);
+            Test("42*_47=1974", -1);
+            Test("2*12_=247", -1);
+        }
+        static void Test(string equation, int expectedAnswer)
+        {
+            int calculatedAnswer = FindMissingNumber(equation);
+            string passOrFails = calculatedAnswer == expectedAnswer ? "PASS" : "FAIL";
+            Console.WriteLine(equation + " " + passOrFails+" calculated answer was " + calculatedAnswer.ToString());
+        }
+        static int FindMissingNumber(string equation)
+        {
             try
             {
                 var terms = equation.Split(new Char[] { '*', '=' });
                 int result = -1;
                 if (terms[0].Contains('_'))
                 {
-                    result=MissingNumberIn1stTerm(terms);
+                    result = MissingNumberIn1stTerm(terms);
                 }
 
                 else if (terms[1].Contains('_'))
                 {
-                   result= MissingNumberIn2ndTerm(terms);
+                    result = MissingNumberIn2ndTerm(terms);
                 }
 
                 else if (terms[2].Contains('_'))
                 {
                     result = MissingNumberIn3rdTerm(terms);
                 }
-                Console.WriteLine(result);
+                return result;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+                return -1;
             }
         }
         static int MissingNumberIn2ndTerm(string[] terms)
@@ -61,7 +75,6 @@ namespace Calculator
             int secondNumber = int.Parse(terms[1]);
             int thirdNumber = int.Parse(terms[2]);
             int firstNumber = thirdNumber / secondNumber;
-            Console.WriteLine(firstNumber);
             int missingNumber=PrintMissingNumber(firstNumber.ToString(), terms[0]);
             return missingNumber;
         }
@@ -71,7 +84,6 @@ namespace Calculator
             int secondNumber = int.Parse(terms[1]);
             int firstNumber = int.Parse(terms[0]);
             int thirdNumber = firstNumber * secondNumber;
-            Console.WriteLine(thirdNumber);
             return PrintMissingNumber(thirdNumber.ToString(), terms[2]);
         }
     }
